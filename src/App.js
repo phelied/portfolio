2f89e4e3a9e1b9222fd8a5b13ba1771009f18be4
kuueli <ophelie.diomar@epitech.eu>
2022-07-05 23:57:58 +0200

    Add content for the about me part

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="container">
       <div className="introduction__display">
         <div className='introduction__text-typing'>
-          Hi,<span className='introduction__name-typing'>ophelie</span> here.
+          Hi,<span className='secondary-color-text'>ophelie</span> here.
         </div>
         <span className='introduction__random-text'>Je crée des choses parfois.</span>
         <p>Je suis une front-end developer residant en Ile-de-France.
@@ -16,6 +16,14 @@ function App() {
           Je m'intéresse au désign, devéloppement web, developpent game et tout ce qu'il y aux milieux.
         </p>
         <div className='introduction__mail-display'><FontAwesomeIcon icon={faEnvelope} />Say hi!</div>
+      </div>
+      <div className="about__display">
+        <span className='tertiary-color-text'>/ about me</span>
+        <p>Je suis actuellement en 2ème année à la <span className='secondary-color-text'>Web@cadémie by Epitech</span> 
+          <br />
+          Je m'intéresse au désign, devéloppement web, developpent game et tout ce qu'il y aux milieux.
+        </p>
+        <div className='about__mail-display'><FontAwesomeIcon icon={faEnvelope} />Say hi!</div>
       </div>
     </div>
   );
