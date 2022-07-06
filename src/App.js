5c52670eb9fc346e978cf85706875b37c3329d62
kuueli <ophelie.diomar@epitech.eu>
2022-07-06 19:49:05 +0200

    Add content for experience

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import './App.css';
-import Links from './components/links'
+import FullWidthTabs from './components/experience'
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faCaretRight } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+
   return (
     <div className="container">
-      <div className="introduction__display">
+      <div id="introduction" className="introduction__display">
         <div className='introduction__text-typing'>
-          Hi,<span className='secondary-color-text'>ophelie</span> here.
+          <div className='introduction__typed-out'>
+            Hi,<span className='secondary-color-text'>ophelie</span> here.
+          </div>
         </div>
         <span className='introduction__random-text'>Je crée des choses parfois.</span>
         <p>Je suis une front-end developer residant en Ile-de-France.
           <br />
           Je m'intéresse au désign, devéloppement web, developpent game et tout ce qu'il y aux milieux.
         </p>
-        <div className='introduction__mail-display'><FontAwesomeIcon icon={faEnvelope} />Say hi!</div>
+        <div className='introduction__mail-display'><a href="mailto: opheliediomar@outlook.fr" ><FontAwesomeIcon icon={faEnvelope} />Say hi!</a></div>
       </div>
-      <div className="about__display">
+      <div id="about" className="about__display">
         <span className='tertiary-color-text'>/ about me</span>
-        <p>Je suis actuellement en 2ème année à la <span className='secondary-color-text'>Web@cadémie by Epitech</span> en alternance.
+        <p className="display-p">Je suis actuellement en 2ème année à la <span className='secondary-color-text'>Web@cadémie by Epitech</span> en alternance.
           Je réalise aujourd'hui mon année d'apprentissage au sein <span className='secondary-color-text'>d'Ubisoft</span> en tant que <em>Front-end developer</em>.
           <br />
           <br />
@@ -34,10 +38,9 @@ function App() {
           </div>
         </p>
       </div>
-      <div className="experience__display">
+      <div id="experience" className="experience__display">
         <span className='tertiary-color-text'>/ experience</span>
-        <p>Je suis actuellement en 2ème année à la <span className='secondary-color-text'>Web@cadémie by Epitech</span> en alternance.
-        </p>
+        <FullWidthTabs />
       </div>
     </div>
   );
