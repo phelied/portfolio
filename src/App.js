341fee9995678aa8411346148200a59012b4c44d
kuueli <ophelie.diomar@epitech.eu>
2023-01-11 21:36:02 +0100

    Add software applications and CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import './App.css';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faCaretRight } from '@fortawesome/free-solid-svg-icons';
-import AwesomeSlider from 'react-awesome-slider';
-import 'react-awesome-slider/dist/styles.css';
-import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
-
+import { faEnvelope, faCaretRight, faFolder, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import weathy from './images/Weathy-screen.png';
 function App() {
+
+  const data = [
+    {
+      title: 'Weathy',
+      description: 'Weathy is a weather app that allows you to check the weather in your city and in the cities of your choice. It also allows you to save your favorite cities and to see the weather forecast for the next 5 days.',
+      image: 'https://i.ibb.co/0nZ3Z3T/weathy.png',
+      link: 'https://weathy.netlify.app/',
+      github: ''
+    },
+    {
+      title: 'portfolio',
+      description: 'Weathy is a weather app that allows you to check the weather in your city and in the cities of your choice. It also allows you to save your favorite cities and to see the weather forecast for the next 5 days.',
+      image: './images/Weathy-screen.png',
+      link: 'https://weathy.netlify.app/',
+      github: ''
+    },
+    {
+      title: 'my auto',
+      description: 'Weathy is a weather app that allows you to check the weather in your city and in the cities of your choice. It also allows you to save your favorite cities and to see the weather forecast for the next 5 days.',
+      image: 'https://i.ibb.co/0nZ3Z3T/weathy.png',
+      link: 'https://weathy.netlify.app/',
+      github: ''
+    },
+  ]
 
   return (
     <div className="container">
       <div id="introduction" className="introduction__display">
         <div className='introduction__text-typing'>
           <div className='introduction__typed-out'>
-            Hi,<span className='secondary-color-text'> ophelie</span> here.
+            Hi,<span className='secondary-color-text'> Ophelie</span> here.
           </div>
         </div>
         <span className='introduction__random-text'>I enjoy creating things.</span>
         <p>I am a full stack developer based in Ile-de-France.
           <br />
-          I have a passion for designing and developing web and game applications. I am always looking to learn and explore new technologies, and am excited to continue growing my skills in a dynamic and fast-paced environment.
+          I have a passion for designing and developing web and game applications.
         </p>
         <div className='introduction__mail-display'><a href="mailto: opheliediomar@outlook.fr" ><FontAwesomeIcon icon={faEnvelope} />Say hi!</a></div>
       </div>
@@ -29,12 +51,12 @@ function App() {
           , as well as a passion for creating dynamic, user-friendly web solutions. I recently completed an
           apprenticeship at <span className='secondary-color-text'>Ubisoft</span>, where I had the
           opportunity to work on various projects and collaborate with a team of experienced developers.
-          I'm a <span className='alt-color'> quick learner</span>, <span className='alt-color'>a team player</span>, and I always strive to create elegant, efficient
+          I'm a <span className='secondary-color-text'> quick learner</span>, <span className='secondary-color-text'>a team player</span>, and I always strive to create elegant, efficient
           and robust code. I'm eager to bring my skills and experience to a new challenge and am
           excited to continue developing my skills in a dynamic and fast-paced work environment.
           <br />
           <br />
-          Voici quelque uns des languages de programmation avec lesquelles j'ai travaillée:
+          Here are some of the programming languages that I have worked with:
           <div className='about__language-display'>
             <span><FontAwesomeIcon icon={faCaretRight} />React.js</span>
             <span><FontAwesomeIcon icon={faCaretRight} />HTML & CSS</span>
@@ -78,13 +100,34 @@ function App() {
       </div>
       <div id="software" className="software__display">
         <span className='tertiary-color-text'>/ software-creations</span>
-        <AwesomeSlider animation="cubeAnimation">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-        </AwesomeSlider>
+        <div className='software__display-projects block-experience__job'>
+          {data.map((project) => (
+            <div key={project.title} className='software__display-project'>
+              <div className='software__display-project-margin'>
+                <div className='software__display-project-nav'>
+                  <FontAwesomeIcon icon={faFolder} />
+                  <div className='software__display-project-links'>
+                    <FontAwesomeIcon icon={faGithub} />
+                    <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                  </div>
+                </div>
+                <div className='software__display-project-info'>
+                  <span className='software__project-title'>{project.title}</span>
+                  <p className='software__project-description'>{project.description}</p>
+                  <span className='software__project-techno'></span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
+      <footer className="footer__display">
+        <div className='footer__display-links'>
+          <a href="https://www.linkedin.com/in/ophelie-diomar-2b1b1b1b1/">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
